aacef7afa89f5ecf7ad5991be54b77f186beeaa0
KentaKurose <s.kuroinu1022@gmail.com>
2022-11-21 12:57:23 +0900

    Fishコントローラの作成 #20

---
+++ app/controllers/fish_controller.rb
@@ -0,0 +1,4 @@
+class FishController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :users, only: %i[new create]
+  resources :fish, only: %i[show]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
