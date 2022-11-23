ae5f34ec0917881c8ef17dcc0950d4984bce5794
KentaKurose <s.kuroinu1022@gmail.com>
2022-11-23 14:29:50 +0900

    水槽の写真 セレクトによる切り替え #20

--- app/javascript/show.js
+++ app/javascript/show.js
@@ -1,20 +1,39 @@
 $(function(){
-  const imgs = ['120x00x60.jpg', '00x60x60.jpg'];
-  let show = 0;
-
-  $('.img').attr('src', '/images/' + imgs[show]);
+  //正面と側面の選択によって表示する画像を変えたい
+  let direction = 'front';
+  let val = 0;
 
   let radio_btns = document.querySelectorAll(`input[type='radio'][name='type']`);
 
   for (let target of radio_btns) {
     target.addEventListener('change', function(){
-      if(show == imgs.length -1){
-        show =0;
+      if(val == 0){
+        direction = 'side';
+        val++;
       }else{
-        show++;
+        direction = 'front';
+        val =0;
       }
-      console.log(show);
-      $('.img').attr('src', '/images/' + imgs[show]);
+      var select_width2 = $('select[name="width"]').val();
+      var select_depth2 = $('select[name="depth"]').val();
+      var select_length2 = $('select[name="length"]').val();
+      $('.img').attr('src', `/images/fish_tanks/${direction}/` + `${select_width2}x${select_depth2}x${select_length2}.jpg`);
     });
   }
+
+  //各セレクトを取得...これを選択済みのオプションのみ取得できるようにしたい
+  let select_width = document.querySelector(`select[name='width'] option[selected='selected']`);
+  let select_depth = document.querySelector(`select[name='depth'] option[selected='selected']`);
+  let select_length = document.querySelector(`select[name='length'] option[selected='selected']`);
+
+  //各選択済みのセレクトのvalueを組み合わせて画像を表示させたい
+  $('.img').attr('src', `/images/fish_tanks/${direction}/` + `${select_width.value}x${select_depth.value}x${select_length.value}.jpg`);
+
+  //セレクタを変更したら画像の変更イベントを発生させたい
+  $('select').change(function(){
+    var select_width2 = $('select[name="width"]').val();
+    var select_depth2 = $('select[name="depth"]').val();
+    var select_length2 = $('select[name="length"]').val();
+    $('.img').attr('src', `/images/fish_tanks/${direction}/` + `${select_width2}x${select_depth2}x${select_length2}.jpg`);
+  });
 });
