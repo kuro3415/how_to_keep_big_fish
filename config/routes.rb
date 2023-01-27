3e473d1f5c9e4056c784bb69a6cd16801b79a4ca
KentaKurose <s.kuroinu1022@gmail.com>
2023-01-27 17:04:38 +0900

    Postコントローラーの作成 #33

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
