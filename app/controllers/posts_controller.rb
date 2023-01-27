6a2900c8811ae03685ac8c59df8f2fa052f08c75
KentaKurose <s.kuroinu1022@gmail.com>
2023-01-28 00:50:19 +0900

    アップローダー追加 #33

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'users/show'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :post_image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  mount_uploader :post_image, PostImageUploader
 end
