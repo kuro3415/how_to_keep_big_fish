161277e47e0177a424fc4fa81e924ab699b1c313
KentaKurose <s.kuroinu1022@gmail.com>
2023-04-08 23:27:25 +0900

    importmapでFontAwesome導入 #43

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-free'
+library.add(fas, far, fab)
