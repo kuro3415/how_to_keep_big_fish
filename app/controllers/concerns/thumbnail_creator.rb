9946ab7923adc48f6b1db806929fbfba74e84a09
KentaKurose <s.kuroinu1022@gmail.com>
2023-04-08 22:47:25 +0900

    合成画像の調整  #43

--- app/controllers/concerns/thumbnail_creator.rb
+++ app/controllers/concerns/thumbnail_creator.rb
@@ -4,10 +4,10 @@ class ThumbnailCreator
   # 文字のサイズや位置
   GRAVITY = 'center'.freeze
   FONT = './app/assets/fonts/Senobi-Gothic-Bold.ttf'.freeze
-  USER_NAME_FONT_SIZE = 60
-  FISH_NAME_FONT_SIZE = 80
-  USER_NAME_TEXT_POSITION = '0, 300'.freeze
-  FISH_NAME_TEXT_POSITION = '0,-130'.freeze
+  USER_NAME_FONT_SIZE = 30
+  FISH_NAME_FONT_SIZE = 60
+  USER_NAME_TEXT_POSITION = '0, 250'.freeze
+  FISH_NAME_TEXT_POSITION = '0,-220'.freeze
   INDENTION_COUNT = 20
   ROW_LIMIT = 2
 
@@ -26,7 +26,7 @@ class ThumbnailCreator
         config.fill 'white'
         config.gravity GRAVITY
         config.pointsize USER_NAME_FONT_SIZE
-        config.draw "text #{USER_NAME_TEXT_POSITION} '#{user_name}'"
+        config.draw "text #{USER_NAME_TEXT_POSITION} 'by #{user_name}'"
       end
       # ユーザーの名前入りの画像に魚の名前を入れる
       image_with_name.combine_options do |config|
