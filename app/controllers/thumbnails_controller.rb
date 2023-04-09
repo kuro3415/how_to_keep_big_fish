9ad036846150dde3a42d666546f8d346b6102959
KentaKurose <s.kuroinu1022@gmail.com>
2023-04-09 23:55:58 +0900

    画像生成とtwitterボタン（仮） #43

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -1,14 +1,6 @@
 class ThumbnailsController < ApplicationController
-  # 例 : 参考にする
-  # def create
-    # @interview = current_user.interviews.build(interview_params)
-    # @interview.thumbnail = ThumbnailCreator.build(@interview.title, @interview.thumbnail, @interview.user.decorate.full_name)
-    # if @interview.save
-      # redirect_to interviews_path
-    # else
-
   def create
-    @post = Post.find(12) # 投稿を持ってくる
+    @post = Post.find(params[:post_id])
     image = ThumbnailCreator.build(@post.user.name, @post.article.fish.name, @post.post_image).tempfile.open.read
     send_data image, :type => 'image/png',:disposition => 'inline'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,12 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show new create]
   resources :articles, only: %i[show index] do
-    resources :posts, only: %i[show create edit update destroy]
+    resources :posts, only: %i[show create edit update destroy] do
+      resources :thumbnails, only: %i[create]
+    end
   end
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-
-  # OGPお試し
-  get 'thumbnails/ogp.png', to: 'thumbnails#create', as: 'images_ogp'
 end
