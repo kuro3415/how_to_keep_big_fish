146a6058a66bef03cb7b14693ece52197cff988b
KentaKurose <s.kuroinu1022@gmail.com>
2023-04-11 01:28:54 +0900

    thumbnail_imageカラム追加 #43

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_061740) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_162344) do
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "fish_id"
     t.datetime "created_at", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_061740) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "thumbnail_image"
     t.index ["post_id"], name: "index_thumbnails_on_post_id"
   end
 
