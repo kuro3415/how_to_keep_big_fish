b98fd1bfefd9e96d1a8592d7d486bb2077aed819
KentaKurose <s.kuroinu1022@gmail.com>
2023-04-14 22:39:13 +0900

    twiiter発火変更 #43

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -2,10 +2,12 @@ class ThumbnailsController < ApplicationController
   before_action :set_twitter_api
 
   def create
-    @thumbnail = Thumbnail.find(2)
-
-    image_path = @thumbnail.thumbnail_image
-
-    redirect_to "https://twitter.com/share?url=#{image_path.to_s}", allow_other_host: true
+    @post = Post.find(params[:post_id])
+    @thumbnail = @post.build_thumbnail
+    @thumbnail.thumbnail_image = ThumbnailCreator.build(@post.user.name, @post.article.fish.name, @post.post_image)
+    if @thumbnail.save
+      image_path = @thumbnail.thumbnail_image
+      redirect_to "https://twitter.com/share?url=#{image_path.to_s}", allow_other_host: true
+    end
   end
 end
